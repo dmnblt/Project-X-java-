00485c9d0aefc1344a4ebf5cd7a20692c783ba50
dmnblt <60312360+dmnblt@users.noreply.github.com>
2022-04-11 23:13:17 +0600

    Add files via upload

---
+++ src/main/java/com/example/learnspring/Service/PostService.java
@@ -0,0 +1,30 @@
+package com.example.learnspring.Service;
+
+import com.example.learnspring.models.Post;
+import com.example.learnspring.repo.PostRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    private final PostRepository postRepository;
+
+    public PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    public List<Post> findAll() {
+        return postRepository.findAll();
+    }
+
+    public Optional<Post> findById(Long id) {
+        return postRepository.findById(id);
+    }
+
+    public Post save(Post post) {
+        return postRepository.save(post);
+    }
+}
---
+++ src/main/java/com/example/learnspring/models/Comment.java
@@ -0,0 +1,27 @@
+package com.example.learnspring.models;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@RequiredArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Comment extends Auditable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NonNull
+    private String body;
+
+    // Post. Mapping: Many to One. Comments -> Post.
+    @ManyToOne
+    @NonNull
+    private Post post;
+
+
+}
