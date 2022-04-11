9a70ba79a579343524d112010bd4750ce701fe1a
dmnblt <60312360+dmnblt@users.noreply.github.com>
2022-04-11 23:08:21 +0600

    Delete src directory

--- src/main/java/com/example/learnspring/repo/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.learnspring.repo;
-
-import com.example.learnspring.models.Post;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PostRepository extends CrudRepository<Post, Long> {
-}
